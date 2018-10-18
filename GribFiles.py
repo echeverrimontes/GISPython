f7aa2440fdbe2417f0c83a71656d06cbf7e0648e
felipunky <feli20gutierrez@hotmail.com>
2018-10-18 19:37:07 +0200

    Add files via upload

--- GribFiles.py
+++ GribFiles.py
@@ -3,12 +3,25 @@ import numpy as np
 np.set_printoptions( threshold = np.nan )
 import struct
 import pandas as pd
+from mpl_toolkits.basemap import Basemap
 
 # Read the GRIB file
-dataset = gdal.Open( 'C:\Users\Paula\Downloads\estofs.mic.t00z.guam.f000.grib2', gdal.GA_ReadOnly )
-band = dataset.GetRasterBand(1)
+dataset = gdal.Open( 'C:\Users\Paula\Downloads\pgbf2018101806.01.2018101806.grb2', gdal.GA_ReadOnly )
+
+count = dataset.RasterCount
+
+for i in range( 1, count ):
+    
+    print( 'Raster Band #: ' + str( i ) + ' has ' + str( dataset.GetRasterBand( i ).XSize ) + ' Rows' )
+    print( 'Raster Band #: ' + str( i ) + ' has ' + str( dataset.GetRasterBand( i ).YSize ) + ' Columns' )
+
+band = dataset.GetRasterBand( 1 )
+
+# Use a C struct to store our band data.
+'''
 pd.set_option( 'display.max_rows', band.YSize )
 pd.set_option( 'display.max_columns', band.XSize )
+'''
 
 '''
 scanline = band.ReadRaster(xoff=0, yoff=0,
@@ -23,10 +36,17 @@ print( ( tuple_of_floats ) )
 
 meta = band.GetMetadata()
 
+#print( dataset.GetProjection() )
+
+#print( dataset.GetProjectionRef() )
+#print( dataset.GetGeoTransform() )
+
 print( meta )
 
-data = band.ReadAsArray().astype( np.float32 )
+# Read as numpy array.
+#data = dataset.ReadAsArray().astype( np.float32 )
 
+# Print using pandas.
 #print( pd.DataFrame.from_records( data ) )
 
 # Number of items in numpy array.
